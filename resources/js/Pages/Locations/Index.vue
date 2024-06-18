e55f916ec0f7b9824cd7c1f088281ac32034deb1
Cora Marcu <coramarcu@gmail.com>
2024-06-18 11:29:20 +0100

    feat: adds basic tailwind styling

--- resources/js/Pages/Locations/Index.vue
+++ resources/js/Pages/Locations/Index.vue
@@ -1,22 +1,25 @@
 <template>
+    <Head title="All Locations" />
     <h1>Welcome to our app</h1>
     {{ console.log(locations) }}
 
     <article v-for="location in locations" :key="location.id">
-        {{ console.log(location.name) }}
-        {{ location.name }}
+        <div class="flex">
+            <h2>{{ location.name }}</h2>
+            <a href="/locations">Edit</a>
+            <button>Delete</button>
+        </div>
     </article>
 </template>
 
 <script>
+import { Head } from "@inertiajs/vue3";
 export default {
     props: {
         locations: {
             required: true,
         },
     },
-    data() {
-        return {};
-    },
+    data() {},
 };
 </script>
