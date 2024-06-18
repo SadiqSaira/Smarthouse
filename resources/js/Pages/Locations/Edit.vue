b426d0860310396eca48dab1187510f41fb092a2
Cora Marcu <coramarcu@gmail.com>
2024-06-18 20:20:51 +0100

    feat: style location edit

--- resources/js/Pages/Locations/Edit.vue
+++ resources/js/Pages/Locations/Edit.vue
@@ -1,26 +1,47 @@
 <template>
     <Head title="Edit location" />
-    <h1>Edit {{ location.name }}</h1>
 
-    <form @submit.prevent="form.post(`/locations/${location.id}`)" method="post">
-        <label for="location-name">Change location name:</label>
-        <input
-            id="location-name"
-            v-model="form.name"
-            type="text"
-            :placeholder="location.name"
-        />
+    <section class="flex flex-col justify-center items-center py-8">
+        <h1 class="pb-8 text-3xl">
+            Edit location:
+            <span class="text-blue-500 font-bold">{{ location.name }}</span>
+        </h1>
 
-        <label for="location_address">Change location name:</label>
-        <input
-            id="location_address"
-            v-model="form.address"
-            type="text"
-            :placeholder="location.address"
-        />
+        <form
+            @submit.prevent="form.post(`/locations/${location.id}`)"
+            method="post"
+            class="flex flex-col justify-center items-center py-8 w-full max-w-3xl"
+        >
+            <label for="location-name" class="w-full"
+                >Change location name:</label
+            >
+            <input
+                id="location-name"
+                v-model="form.name"
+                type="text"
+                :placeholder="location.name"
+                class="w-full border-inherit border-solid border-2 rounded-lg mb-16 py-4 px-8"
+            />
 
-        <button type="submit">Submit</button>
-    </form>
+            <label for="location_address" class="w-full"
+                >Change location address:</label
+            >
+            <input
+                id="location_address"
+                v-model="form.address"
+                type="text"
+                :placeholder="location.address"
+                class="w-full border-inherit border-solid border-2 rounded-lg mb-4 py-4 px-8"
+            />
+
+            <button
+                type="submit"
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4 border-2 border-blue-500 hover:border-blue-700"
+            >
+                Submit
+            </button>
+        </form>
+    </section>
 </template>
 
 <script>
