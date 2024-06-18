f50469cff07bc8a9cdb6c898afbead3fea0cc034
Cora Marcu <coramarcu@gmail.com>
2024-06-18 15:41:50 +0100

    feat: add form to location edit

--- resources/js/Pages/Locations/Edit.vue
+++ resources/js/Pages/Locations/Edit.vue
@@ -1,10 +1,35 @@
 <template>
     <Head title="Edit location" />
     <h1>Edit {{ location.name }}</h1>
+
+    <form @submit.prevent="form.post(``)" method="post">
+        <label for="location-name">Change location name:</label>
+        <input
+            id="location-name"
+            v-model="form.name"
+            type="text"
+            :placeholder="location.name"
+        />
+
+        <label for="location_address">Change location name:</label>
+        <input
+            id="location_address"
+            v-model="form.address"
+            type="text"
+            :placeholder="location.address"
+        />
+
+        <button type="submit">Submit</button>
+    </form>
 </template>
 
 <script>
-import { Head } from "@inertiajs/vue3";
+import { Head, useForm } from "@inertiajs/vue3";
+
+const form = useForm({
+    name: "",
+    address: "",
+});
 
 export default {
     props: {
@@ -12,6 +37,10 @@ export default {
             required: true,
         },
     },
-    data() {},
+    data() {
+        return {
+            form: form,
+        };
+    },
 };
 </script>
