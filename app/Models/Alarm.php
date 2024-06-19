19ffdf06d3fb7b3d2e1f77a99194ac0afe21bfdc
SadiqSaira <sairaishaq@gmail.com>
2024-06-19 19:06:27 +0100

    refactor: Models

--- app/Models/Alarm.php
+++ app/Models/Alarm.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class Alarm extends Model
 {
@@ -11,7 +12,7 @@ class Alarm extends Model
     protected $fillable = [
         'status',
     ];
-    public function device()
+    public function device(): MorphOne
     {
         return $this->morphOne(Device::class, 'deviceable');
     }
--- app/Models/Speaker.php
+++ app/Models/Speaker.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class Speaker extends Model
 {
@@ -11,7 +12,7 @@ class Speaker extends Model
     protected $fillable = [
         'volume',
     ];
-    public function device()
+    public function device(): MorphOne
     {
         return $this->morphOne(Device::class, 'deviceable');
     }
