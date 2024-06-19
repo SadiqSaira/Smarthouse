14543ebf68b1b726b4d30f5523b7a084c8ce99ee
SadiqSaira <sairaishaq@gmail.com>
2024-06-19 18:43:50 +0100

    test: Added feature tests for room CRUD operations in backend

---
+++ tests/Feature/Services/RoomServiceTest.php
@@ -0,0 +1,120 @@
+<?php
+namespace Tests\Unit\Services;
+
+use App\Services\RoomService;
+use App\Services\LocationService;
+use Tests\TestCase;
+use App\Models\Location;
+use App\Models\Room;
+use App\Http\Requests\RoomRequest;
+use App\Http\Requests\LocationRequest;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Log;
+
+class RoomServiceTest extends TestCase
+{
+    use RefreshDatabase;
+    
+  
+    private $roomService;
+    private $locationService;
+    private $location;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->roomService = new RoomService();
+        $this->locationService = new LocationService();
+        $this->createLocation();
+    }
+    public function createLocation(){
+        $locationRequest = new LocationRequest([
+            'name' => 'Test Location 1',
+            'address' => 'Test address 1',
+        ]);
+        $this->locationService->add($locationRequest);
+        $this->location = Location::first();
+    }
+
+
+    public function test_it_can_add_a_room()
+    {
+
+        // Assert the location was created
+        $this->assertDatabaseHas('locations', [
+            'id' => $this->location->id,
+            'name' => 'Test Location 1',
+            'address' => 'Test address 1',
+        ]);
+
+        // Create room request
+        $roomRequest = new RoomRequest([
+            'name' => 'Test Room',
+            'location_id' => $this->location->id,
+        ]);
+        
+        // Add room
+        $this->roomService->add($roomRequest);
+        $room = Room::first(); 
+
+        // Assert the room was created
+        $this->assertDatabaseHas('rooms', [
+            'id' => $room->id,
+            'name' => 'Test Room',
+            'location_id' => $this->location->id,
+        ]);
+
+        $this->refreshDatabase();
+    }
+    public function test_it_can_update_a_room(){
+
+        // Create room request
+        $roomRequest = new RoomRequest([
+            'name' => 'Test Room',
+            'location_id' => $this->location->id,
+        ]);
+        Log::info('Updating room with request:', $roomRequest->all());
+
+        // Add room
+        $this->roomService->add($roomRequest);
+        $room = Room::first(); 
+        $roomRequest = new RoomRequest([
+            'id' => $room->id,
+            'name' => 'Test Room update',
+            'location_id' => $this->location->id,
+        ]);
+        $this->roomService->add($roomRequest);
+        // Assert the room was created
+        $this->assertDatabaseHas('rooms', [
+            'id' => $room->id,
+            'name' => 'Test Room update',
+            'location_id' => $this->location->id,
+        ]);
+
+    }
+    public function test_it_can_delete_a_room(){
+
+        // Create room request
+        $roomRequest = new RoomRequest([
+            'name' => 'Test Room',
+            'location_id' => $this->location->id,
+        ]);
+
+        // Add room
+        $this->roomService->add($roomRequest);
+        //get the recently created room
+        $room = Room::first(); 
+
+        Room::where('id', $room->id)->delete();
+        
+        // Assert the room was created
+        $this->assertDatabaseMissing('rooms', [
+            'id' => $room->id,
+        ]);
+
+    }
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+    }
+}
