c94ff783d30d576415f984a35e9ae1dafb28cf9d
SadiqSaira <sairaishaq@gmail.com>
2024-06-19 09:04:43 +0100

    feat: Added backend for Room. RoomController.php, RoomRequest.php, RoomService.php and changed in web.php

---
+++ app/Services/RoomService.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Services;
+
+use App\Models\Room;
+use App\Http\Requests\RoomRequest;
+use Illuminate\Support\Facades\Log;
+
+class RoomService {
+    public function __construct() {}
+
+    public function getRoomsByLocationId($location_id) {
+        $rooms = Room::where('location_id', $location_id)->get();
+        // Log::info('All locations', $locations);
+
+        return $rooms;
+    }
+
+    public function getRoomById($id)
+    {
+        // Log::info($locationRequest['locationId']);
+
+        $room = Room::findOrFail($id);
+        return $room;
+    }
+
+    public function add(RoomRequest $roomRequest) {
+
+        // Log::info('Location Updated or Created:', [
+        //     'location id' => $locationRequest['id'],
+        // ]);
+        Room::updateOrCreate(
+            ['id' => (int)$roomRequest['id']],
+            
+            [
+            'name' => $roomRequest['name'],
+            'location_id' => $roomRequest['location_id'],
+            ]
+    );
+    }
+    public function delete(RoomRequest $roomRequest){
+        Room::where('id', $roomRequest['id'])->delete();    
+    }
+
+}
